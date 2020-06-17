f2745fe0b5acf5dd756c0e8149753f829da4db28
Erwin Sha <erwin.sha@sap.com>
2020-06-17 08:56:49 +0000

    Update by Erwin

--- webapp/controller/App5.controller.js
+++ webapp/controller/App5.controller.js
@@ -1,34 +1,33 @@
 sap.ui.controller("IBSO.ALP_DEMO.controller.App5", {
 	onInit: function () {
-		
+
 		var firstDay = new Date(2020, 0, 1);
-		
+
 		function totalWeeks() {
 			var endYear = new Date(firstDay.getFullYear(), 11, 31, 23, 59, 59, 999);
-			var msPerWeek = 7* 24 * 60 * 60 * 1000;
+			var msPerWeek = 7 * 24 * 60 * 60 * 1000;
 			var weeks = Math.round((endYear.getTime() - firstDay.getTime()) / msPerWeek);
-			
-            function getTotalWeeks() {
-            	return weeks;
-            }
-            
-            return getTotalWeeks;
-		};
-		
+
+			function getTotalWeeks() {
+				return weeks;
+			}
+
+			return getTotalWeeks;
+		}
+
 		var result = totalWeeks();
 		var finalWeeks = result();
-		
+
 		var aColumnData = [];
 		var col = "cw";
-		for (var x = 0; x < finalWeeks; x++) {
-			col = col + x;
-			// aColumnData.columnName = col.concat("Week", x);
-			aColumnData[x] = {
-				columnName: col
-			};
-			col = "cw";
-		};
-		
+		// for (var x = 1; x < finalWeeks; x++) {
+		// 	col = col + x;
+		// 	aColumnData[x] = {
+		// 		columnName: col
+		// 	};
+		// 	col = "cw";
+		// }
+
 		var aRowData = [];
 		var item = {};
 		for (var i = 0; i < 5; i++) {
@@ -37,48 +36,10 @@ sap.ui.controller("IBSO.ALP_DEMO.controller.App5", {
 				item[col] = col.concat("Row", i);
 				col = "cw";
 			}
-			
+
 			aRowData.push(item);
 			item = {};
-		};
-		
-
-		var columnData = [{
-			columnName: "supplier"
-		}, {
-			columnName: "street"
-		}, {
-			columnName: "city"
-		}, {
-			columnName: "phone"
-		}];
-
-		var rowData = [{
-			supplier: "Titanium",
-			street: "401 23rd St",
-			city: "Port Angeles",
-			phone: "5682-121-828"
-		}, {
-			supplier: "Technocom",
-			street: "51 39th St",
-			city: "Smallfield",
-			phone: "5682-121-826"
-		}, {
-			supplier: "Titanium",
-			street: "40 21st St",
-			city: "Meridian",
-			phone: "5682-121-827"
-		}, {
-			supplier: "Red Point Stores",
-			street: "451 55th St",
-			city: "Bethesda",
-			phone: "5682-121-829"
-		}, {
-			supplier: "Very Best Screens",
-			street: "123 72nd St",
-			city: "McLean",
-			phone: "5682-121-829"
-		}];
+		}
 
 		var oTable = this.getView().byId("DYNTAB");
 		/*var oTable = new sap.ui.table.Table({
@@ -94,28 +55,143 @@ sap.ui.controller("IBSO.ALP_DEMO.controller.App5", {
 			columns: aColumnData
 		});
 
-		oTable.setModel(oModel);
+		// oTable.setModel(oModel);
 
-		oTable.bindColumns("/columns", function (sId, oContext) {
-			var columnName = oContext.getObject().columnName;
-			return new sap.ui.table.Column({
-				label: columnName,
-				template: columnName,
+		oTable.addColumn(new sap.ui.table.Column({
+			label: new sap.m.Label({
+				text: "zone"
+			}),
+			template: new sap.m.Label({
+				text: "{zone}"
+			}),
+			sortProperty: "zone"
+		}));
+
+		oTable.addColumn(new sap.ui.table.Column({
+			label: new sap.m.Label({
+				text: "country"
+			}),
+			multiLabels: [
+				new sap.m.Label({
+					text: "Month"
+				}),
+				new sap.m.Label({
+					text: "Week"
+				})
+			],
+			template: new sap.m.Label({
+				text: "{country}"
+			})
+		}));
+
+		oTable.addColumn(new sap.ui.table.Column({
+			label: new sap.m.Label({
+				text: "total",
+				textAlign: "Center",
+				width: "100%"
+			}),
+			template: new sap.m.Label({
+				text: "{total}"
+			})
+		}));
+
+		var multiCol = "2020";
+		var z = "0";
+
+		for (var x = 1; x < finalWeeks; x++) {
+			col = col + x;
+			aColumnData[x] = {
+				columnName: col
+			};
+
+			if ((x - 1) % 4 === 0) {
+				z++;
+				switch (z) {
+				case 1:
+					multiCol = multiCol.concat("Jan");
+					break;
+				case 2:
+					multiCol = multiCol.concat("Feb");
+					break;
+				case 3:
+					multiCol = multiCol.concat("Mar");
+					break;
+				case 4:
+					multiCol = multiCol.concat("Apr");
+					break;
+				case 5:
+					multiCol = multiCol.concat("May");
+					break;
+				case 6:
+					multiCol = multiCol.concat("Jun");
+					break;
+				case 7:
+					multiCol = multiCol.concat("Jul");
+					break;
+				case 8:
+					multiCol = multiCol.concat("Aug");
+					break;
+				case 9:
+					multiCol = multiCol.concat("Sep");
+					break;
+				case 10:
+					multiCol = multiCol.concat("Oct");
+					break;
+				case 11:
+					multiCol = multiCol.concat("Nov");
+					break;
+				case 12:
+					multiCol = multiCol.concat("Dec");
+					break;
+				}
+
+			}
+
+			oTable.addColumn(new sap.ui.table.Column({
+				label: new sap.m.Label({
+					text: col
+				}),
 				multiLabels: [
 					new sap.m.Label({
-						text: "Contact",
-						textAlign:"Center"
+						text: multiCol
 					}),
 					new sap.m.Label({
-						text: columnName
+						text: col,
+						textAlign: "Center",
+						width: "100%"
 					})
 				],
-				sortProperty: columnName,
-				filterProperty: columnName,
+				template: new sap.m.Input({
+					value: col
+				}),
 				headerSpan: 4
-			});
-		});
+			}));
 
+			col = "cw";
+			multiCol = "2020";
+		}
+
+		// oTable.bindColumns("/columns", function (sId, oContext) {
+		// 	var columnName = oContext.getObject().columnName;
+		// 	return new sap.ui.table.Column({
+		// 		label: columnName,
+		// 		template: columnName,
+		// 		multiLabels: [
+		// 			new sap.m.Label({
+		// 				text: "Contact",
+		// 				textAlign:"Center"
+		// 			}),
+		// 			new sap.m.Label({
+		// 				text: columnName
+		// 			})
+		// 		],
+		// 		sortProperty: columnName,
+		// 		filterProperty: columnName,
+		// 		headerSpan: 4
+		// 	});
+		// });
+
+		oTable.setModel(oModel);
 		oTable.bindRows("/rows");
 
 	}
